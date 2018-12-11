4b1f16cfd8a5edee761ef28d10fe18292156e31b
Rclark4598 <rickdclark48@gmail.com>
2018-12-11 13:04:25 -0700

    burger_logic

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+var orm = require("../config/orm");
+
+module.exports = {
+  getBurgerList: (callback) => orm.selectAll("burgers", callback),
+  insertBurger: (burgerName, callback) => orm.insertOne(
+    "burgers",
+    {
+      burger_name: burgerName,
+      devoured: false
+    },
+    callback
+  ),
+  updateBurger: (burgerId, callback) => {
+    orm.updateOne(
+      "burgers",
+      { devoured: true },
+      { id: burgerId },
+      callback
+    );
+  }
+}
