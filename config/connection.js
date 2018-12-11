6d6191afe6017c174c1117fbcbde9db8935b38d9
Rclark4598 <rickdclark48@gmail.com>
2018-12-11 16:52:16 -0700

    another_one

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,17 @@
 var mysql = require("mysql");
-
 var connection;
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// }
-// else {
-//   connection = mysql.createConnection({
-//     port: 3306,
-//     user: "root",
-//     password: process.env.DB_PASS,
-//     database: "burgers_db"
-//   });
-// }
-
-connection = mysql.createConnection({
-  host: "xefi550t7t6tjn36.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  user: "mu9xskgdpnr2f5ls",
-  password: process.env.JAWSDB_PASS,
-  port: 3306,
-  database: "eefb7d3abjstwrf5"
-  
-})
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    port: 3306,
+    user: "root",
+    password: process.env.DB_PASS,
+    database: "burgers_db"
+  });
+}
 
 connection.connect(error => {
   if (error) return console.error(error);
