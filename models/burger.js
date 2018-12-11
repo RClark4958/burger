0e9a0b2f588d905825ef07cbaef980f95b893e0a
Rclark4598 <rickdclark48@gmail.com>
2018-12-11 16:31:59 -0700

    mysl_three

--- models/burger.js
+++ models/burger.js
@@ -1,9 +1,9 @@
 var orm = require("../config/orm");
 
 module.exports = {
-  getBurgerList: (callback) => orm.selectAll("burgers_db.burgers", callback),
+  getBurgerList: (callback) => orm.selectAll("burgers", callback),
   insertBurger: (burgerName, callback) => orm.insertOne(
-    "burgers_db.burgers",
+    "burgers",
     {
       burger_name: burgerName,
       devoured: false
@@ -12,7 +12,7 @@ module.exports = {
   ),
   updateBurger: (burgerId, callback) => {
     orm.updateOne(
-      "burgers_db.burgers",
+      "burgers",
       { devoured: true },
       { id: burgerId },
       callback
