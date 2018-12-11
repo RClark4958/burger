29dc3485a20c6172145644e7b37505ca3679ba23
Rclark4598 <rickdclark48@gmail.com>
2018-12-11 12:38:26 -0700

    connection

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+var mysql = require("mysql");
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    port: 3306,
+    user: "root",
+    password: process.env.MY_SQL_PASSWORD ? process.env.MY_SQL_PASSWORD : "",
+    database: "burgers_db"
+  });
+}
+
+connection.connect(error => {
+  if (error) return console.error(error);
+  console.log("connected as id" + connection.threadId);
+});
+
+module.exports = connection;
