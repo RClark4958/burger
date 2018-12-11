3098f1ae89e5180a84cc0335ba8a3f037573cbd9
Rclark4598 <rickdclark48@gmail.com>
2018-12-11 12:51:41 -0700

    orm

---
+++ config/orm.js
@@ -0,0 +1,22 @@
+var connection = require("./connection");
+
+module.exports = {
+  selectAll: (tableName, callback) => {
+    connection.query("SELECT * FROM ??", [tableName], (error, results) => {
+      if (error) return console.error(error);
+      callback(results);
+    });
+  },
+  insertOne: (tableName, entryObject, callback) => {
+    connection.query("INSERT INTO ?? SET ?", [tableName, entryObject], (error, results) => {
+      if (error) return console.error(error);
+      callback(results);
+    });
+  },
+  updateOne: (tableName, updatedPropertiesObject, idObject, callback) => {
+    connection.query("UPDATE ?? SET ? WHERE ?", [tableName, updatedPropertiesObject, idObject], (error, results) => {
+      if (error) return console.error(error);
+      callback(results);
+    });
+  }
+}
