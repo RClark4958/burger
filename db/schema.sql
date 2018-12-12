a2975d2d18f0d2ea866ec1a85dd84d0164845224
Rclark4598 <rickdclark48@gmail.com>
2018-12-11 17:09:11 -0700

    sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,9 @@
--- DROP DATABASE IF EXISTS burgers_db;
--- CREATE DATABASE burgers_db;
+DROP DATABASE IF EXISTS eefb7d3abjstwrf5;
+CREATE DATABASE eefb7d3abjstwrf5;
 
 USE eefb7d3abjstwrf5;
 
-CREATE TABLE burgers(
+CREATE TABLE eefb7d3abjstwrf5.burgers(
     id INT(4) AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(60) NOT NULL,
     devoured BOOLEAN NOT NULL,
